0b520f3b66952d413308ba3dfed72e31a84d78e8
Sergio Francisco <spinheiro.lf@gmail.com>
2022-01-06 22:04:15 -0300

    resetando o jogo

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const colorCode = document.getElementById('rgb-color');
 const circles = document.getElementsByClassName('ball');
+const gameText = document.getElementById('answer');
+const reset = document.getElementById('reset');
 
 function generateCode() {
   const color1 = Math.floor(Math.random() * 255);
@@ -8,24 +10,40 @@ function generateCode() {
   colorCode.innerText = `(${color1}, ${color2}, ${color3})`;
 }
 
-
 function fillCircles() {
   const answer = Math.floor(Math.random() * 6);
   const teste = colorCode.innerText;
   for (let i = 0; i < circles.length; i += 1) {
     if (i === answer) {
       circles[i].style.backgroundColor = `rgb${teste}`;
-      circles[i].classList.add('answer')
-    }
-    else {
+    } else {
       circles[i].style.backgroundColor = `#${Math.floor(Math.random() * 16777000).toString(16)}`;
     }
   }
 }
 
-function start (){
+function colorGuess() {
+  if (event.target.style.backgroundColor === `rgb${colorCode.innerText}`) {
+    gameText.innerText = 'Acertou';
+  } else {
+    gameText.innerText = 'Errou! Tente novamente!';
+  }
+}
+function resetGame() {
   generateCode();
   fillCircles();
+  gameText.innerText = 'Escolha uma cor';
+}
+function listenAll() {
+  for (let i = 0; i < circles.length; i += 1) {
+    circles[i].addEventListener('click', colorGuess);
+  }
+}
+function start() {
+  generateCode();
+  fillCircles();
+  listenAll();
 }
 
 window.onload = start;
+reset.addEventListener('click', resetGame);
