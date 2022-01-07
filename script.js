56bc09247f493b6d2df99ff51353c8b63cdd510c
Sergio Francisco <spinheiro.lf@gmail.com>
2022-01-06 21:22:44 -0300

    sorteando cores com resposta

--- script.js
+++ script.js
@@ -12,13 +12,16 @@ window.onload = generateCode;
 
 function fillCircles() {
   const answer = Math.floor(Math.random() * 6);
-  let teste = colorCode.innerText;
+  const teste = colorCode.innerText;
   console.log(teste);
+  console.log(answer);
   for (let i = 0; i < circles.length; i += 1) {
     if (i === answer) {
-      circles[i].style.backgroundColor = `rgb (${teste})`;
+      circles[i].style.backgroundColor = `rgb${teste}`;
     }
-    circles[i].style.backgroundColor = `#${Math.floor(Math.random() * 16777000).toString(16)}`;
+    else {
+      circles[i].style.backgroundColor = `#${Math.floor(Math.random() * 16777000).toString(16)}`;
+    }
   }
 }
 colorCode.addEventListener('click', fillCircles);
