68d89649d62f2c41a56535627992282b00a16d54
Sergio Francisco <spinheiro.lf@gmail.com>
2022-01-06 21:38:30 -0300

    requisitos 1-4 funcionando

--- script.js
+++ script.js
@@ -8,20 +8,24 @@ function generateCode() {
   colorCode.innerText = `(${color1}, ${color2}, ${color3})`;
 }
 
-window.onload = generateCode;
 
 function fillCircles() {
   const answer = Math.floor(Math.random() * 6);
   const teste = colorCode.innerText;
-  console.log(teste);
-  console.log(answer);
   for (let i = 0; i < circles.length; i += 1) {
     if (i === answer) {
       circles[i].style.backgroundColor = `rgb${teste}`;
+      circles[i].classList.add('answer')
     }
     else {
       circles[i].style.backgroundColor = `#${Math.floor(Math.random() * 16777000).toString(16)}`;
     }
   }
 }
-colorCode.addEventListener('click', fillCircles);
+
+function start (){
+  generateCode();
+  fillCircles();
+}
+
+window.onload = start;
